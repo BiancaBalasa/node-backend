91fbaa6fe8386c36be72e85f986af1ebf8117d94
Bianca Balasa <balasaandreabianca@gmail.com>
2023-01-09 23:17:07 +0200

    changed database to sqlite

--- config/config.js
+++ config/config.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
 module.exports = {
-  host: "localhost",
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  dialect: "mysql",
+  dialect: "sqlite",
+  storage: process.env.DB_PATH,
 };
