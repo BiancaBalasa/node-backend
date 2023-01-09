13d25d47bc231c5ed4772445078ef4a8a737f584
Bianca Balasa <balasaandreabianca@gmail.com>
2023-01-09 20:27:10 +0200

    resolved register customer

--- graphql/user/mutations.js
+++ graphql/user/mutations.js
@@ -1,69 +1,77 @@
-const { GraphQLString, GraphQLID, GraphQLNonNull } = require("graphql")
-const UserType = require("./typeDef")
-const db = require('../../models')
-const User = require('../../models/user')
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { GraphQLString, GraphQLNonNull } = require("graphql");
+const { UserType, RegisterReturnType } = require("./typeDef");
+const db = require("../../models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const createUser = {
-    type: UserType,
-    args: {
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
-        customerProfileId: { type: GraphQLID },
-        roleId: { type: GraphQLID }
-    },
-    resolve: async (parent, args, context, info) => {
-        const user = await db.User.create(args)
-        return user
-    }
-}
+const registerCustomer = {
+  type: RegisterReturnType,
+  args: {
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    phoneNumber: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+  },
+  resolve: async (parent, args, context, info) => {
+    const customerProfile = await db.CustomerProfile.create({
+      firstName: args.firstName,
+      lastName: args.lastName,
+      phoneNumber: args.phoneNumber,
+    });
+    console.log(customerProfile.dataValues);
+    const user = await db.User.create({
+      email: args.email,
+      password: args.password,
+      customerProfileId: customerProfile.dataValues.id,
+      roleId: "1",
+    });
+    return { user, customerProfile };
+  },
+};
 
 const loginMutation = {
-    type: UserType,
-    args: {
-        email: {
-            type: new GraphQLNonNull(GraphQLString),
-        },
-        password: {
-            type: new GraphQLNonNull(GraphQLString),
-        },
+  type: UserType,
+  args: {
+    email: {
+      type: new GraphQLNonNull(GraphQLString),
     },
-    resolve: async (source, args) => {
-        const {
-          email,
-          password
-        } = args;
-      
-        const user = await db.User.findOne({
-          where: {
-            email,
-          }
-        });
-      
-        if(!user) {
-          return {
-            token: null,
-          }
-        }
-      
-        const passwordMatches = await bcrypt.compare(password, user.password);
-      
-        if(passwordMatches) {
-          const token = jwt.sign({ userID: user.id }, process.env.JWT_SECRET);
-      
-          return {
-            token,
-          }
-        }
-      
-        return {
-          token: null,
-        }
-      }
-}
+    password: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  resolve: async (source, args) => {
+    const { email, password } = args;
+
+    const user = await db.User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return {
+        token: null,
+      };
+    }
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (passwordMatches) {
+      const token = jwt.sign({ userID: user.id }, process.env.JWT_SECRET);
+
+      return {
+        token,
+      };
+    }
+
+    return {
+      token: null,
+    };
+  },
+};
 
 module.exports = {
-    createUser,
-    loginMutation
-}
+  registerCustomer,
+  loginMutation,
+};
--- graphql/user/typeDef.js
+++ graphql/user/typeDef.js
@@ -1,14 +1,32 @@
-const { GraphQLObjectType, GraphQLString, GraphQLID } = require("graphql")
+const { GraphQLObjectType, GraphQLString, GraphQLID } = require("graphql");
 
 const UserType = new GraphQLObjectType({
-    name: "User",
-    fields: () => ({
-        id: { type: GraphQLID },
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
-        customerProfileId: { type: GraphQLID },
-        roleId: { type: GraphQLID }
-    })
-})
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLID },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    customerProfileId: { type: GraphQLID },
+    roleId: { type: GraphQLID },
+  }),
+});
 
-module.exports = UserType
+const CustomerProfileType = new GraphQLObjectType({
+  name: "CustomerProfile",
+  fields: () => ({
+    id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    phoneNumber: { type: GraphQLString },
+  }),
+});
+
+const RegisterReturnType = new GraphQLObjectType({
+  name: "RegisterReturn",
+  fields: () => ({
+    user: { type: UserType },
+    customerProfile: { type: CustomerProfileType },
+  }),
+});
+
+module.exports = { UserType, RegisterReturnType };
